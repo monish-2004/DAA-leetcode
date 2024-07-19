744ce788f84aee060705217a40e8e7d634673ce4
MONISH.A <153350680+monish-2004@users.noreply.github.com>
2024-07-19 05:51:23 +0530

    prg12.java

---
+++ prg12.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();  
+            }
+        }
+        return minHeap.peek();
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int kthLargest = findKthLargest(nums, k);
+        System.out.println("The " + k + "th largest element is: " + kthLargest);
+    }
+}
